e45812c1950be5915f87501e035ec63eb72c2f02
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-28 16:52:47 +0530

    [Tushar Vidwans] Calculating Employee Monthly Wage

--- EmpWage.cs
+++ EmpWage.cs
@@ -8,40 +8,48 @@ namespace EmpWage_Csharp_Day8
 {
     public class EmpWage
     {
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+        const int MAX_WORK_DAYS = 20;
+        const int EMP_WAGE_PER_HOUR = 20;
+
         /// <summary>
         /// This program is used to check whether employee is present or absent
         /// </summary>
         public void empcheck()
         {
 
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-
-            int EMP_WAGE_PER_HOUR = 20;
+            int total_EmpWage = 0;
             int Emp_Daily_hour;
             Random r = new Random();
 
-            int check = r.Next(3);
-
-            switch (check)
+            for (int i = 0; i < MAX_WORK_DAYS; i++)
             {
-                case IS_FULL_TIME:
-                    Console.WriteLine("Employee is Present");
-                    Emp_Daily_hour = 8;
-                    break;
-                case IS_PART_TIME:
-                    Console.WriteLine("Employee is Part time");
-                    Emp_Daily_hour = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    Emp_Daily_hour = 0;
-                    break;
+                int check = r.Next(3);
+
+                switch (check)
+                {
+                    case IS_FULL_TIME:
+                        Console.WriteLine("Employee is Present");
+                        Emp_Daily_hour = 8;
+                        break;
+                    case IS_PART_TIME:
+                        Console.WriteLine("Employee is Part time");
+                        Emp_Daily_hour = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        Emp_Daily_hour = 0;
+                        break;
+                }
+
+
+                int DailyWage = EMP_WAGE_PER_HOUR * Emp_Daily_hour;
+                Console.WriteLine($"Emp daily wage is: {DailyWage}");
+                total_EmpWage += DailyWage;
+
             }
-
-
-            int DailyWage = EMP_WAGE_PER_HOUR * Emp_Daily_hour;
-            Console.WriteLine($"Emp daily wage is: {DailyWage}");
+            Console.WriteLine($"Total Wage :{total_EmpWage}");
         }
 
     }
