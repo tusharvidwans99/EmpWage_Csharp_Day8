6a33c194893f0f515741232b7c208f4f63902b35
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-28 16:23:50 +0530

    [Tushar Vidwans] Adding Welcome message and description about this project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EmpWage_Csharp_Day8
+{
+    public class Program
+    {
+        /// <summary>
+        /// Greeting message to welcome in this program.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            
+        }
+
+    }
+}
