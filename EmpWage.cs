3af0496391d986f5b508d685e887f862aca6b98c
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-28 16:36:56 +0530

    [Tushar Vidwans] Calculating Employee Daily wage & Full time

--- EmpWage.cs
+++ EmpWage.cs
@@ -11,22 +11,31 @@ namespace EmpWage_Csharp_Day8
         /// <summary>
         /// This program is used to check whether employee is present or absent
         /// </summary>
+        
+
         public void empcheck()
         {
 
-            int isPresent = 1;
+            int isFullTime = 1;
+            int EMP_WAGE_PER_HOUR = 20;
+            int Emp_Daily_hour;
             Random r = new Random();
+
             int check = r.Next(2);
 
-            if (check == isPresent)
+            if (check == isFullTime)
             {
                 Console.WriteLine("Employee is Present");
+                Emp_Daily_hour = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                Emp_Daily_hour = 0;
             }
 
+            int DailyWage = EMP_WAGE_PER_HOUR * Emp_Daily_hour;
+            Console.WriteLine($"Emp daily wage is: {DailyWage}");
         }
 
     }
