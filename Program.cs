251a2bbee230c88abcd134df0eab8d0b522f6d2b
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-28 20:13:08 +0530

    [Tushar Vidwans] Checking Employee Wage for multiple companies using Constructor

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EmpWage_Csharp_Day8
+{
+    public class Program
+    {
+        /// <summary>
+        /// Greeting message to welcome in this program.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Computation Program\n");
+            EmpWage Apple = new EmpWage(20, 150, 40, "Apple");
+            EmpWage Microsoft = new EmpWage(20, 140, 38, "Microsoft");
+            EmpWage Google = new EmpWage(18, 145, 42, "Google");
+
+            
+        }
+
+    }
+}
